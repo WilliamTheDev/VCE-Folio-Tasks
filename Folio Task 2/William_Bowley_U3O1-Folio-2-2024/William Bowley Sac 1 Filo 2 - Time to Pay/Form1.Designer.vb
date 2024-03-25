9b13671333dacc6e8353aea1aaa99cc5fbebe7a3
William <69966592+WilliamTheDev@users.noreply.github.com>
2024-03-25 17:15:30 +1100

    Add files via upload
    FT2 Folio name is spell wrong can't be bored to change it.

---
+++ Folio Task 2/William_Bowley_U3O1-Folio-2-2024/William Bowley Sac 1 Filo 2 - Time to Pay/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Time2Pay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TxtEmployeeName = New TextBox()
+        TxtDepartment = New TextBox()
+        TxtHours = New TextBox()
+        TxtHourlyRate = New TextBox()
+        LabEmployeeName = New Label()
+        LabDepartment = New Label()
+        LabHours = New Label()
+        LabHourlyRate = New Label()
+        BtnAdd = New Button()
+        BtnDisplay = New Button()
+        BtnPayReport = New Button()
+        BtnSearch = New Button()
+        BtnClear = New Button()
+        LstDisplay = New ListBox()
+        LabTitle = New Label()
+        SuspendLayout()
+        ' 
+        ' TxtEmployeeName
+        ' 
+        TxtEmployeeName.Location = New Point(642, 228)
+        TxtEmployeeName.Name = "TxtEmployeeName"
+        TxtEmployeeName.Size = New Size(200, 39)
+        TxtEmployeeName.TabIndex = 0
+        ' 
+        ' TxtDepartment
+        ' 
+        TxtDepartment.Location = New Point(642, 297)
+        TxtDepartment.Name = "TxtDepartment"
+        TxtDepartment.Size = New Size(200, 39)
+        TxtDepartment.TabIndex = 1
+        ' 
+        ' TxtHours
+        ' 
+        TxtHours.Location = New Point(642, 372)
+        TxtHours.Name = "TxtHours"
+        TxtHours.Size = New Size(200, 39)
+        TxtHours.TabIndex = 2
+        ' 
+        ' TxtHourlyRate
+        ' 
+        TxtHourlyRate.Location = New Point(642, 440)
+        TxtHourlyRate.Name = "TxtHourlyRate"
+        TxtHourlyRate.Size = New Size(200, 39)
+        TxtHourlyRate.TabIndex = 3
+        ' 
+        ' LabEmployeeName
+        ' 
+        LabEmployeeName.AutoSize = True
+        LabEmployeeName.Location = New Point(364, 235)
+        LabEmployeeName.Name = "LabEmployeeName"
+        LabEmployeeName.Size = New Size(195, 32)
+        LabEmployeeName.TabIndex = 4
+        LabEmployeeName.Text = "Employee Name:"
+        ' 
+        ' LabDepartment
+        ' 
+        LabDepartment.AutoSize = True
+        LabDepartment.Location = New Point(364, 307)
+        LabDepartment.Name = "LabDepartment"
+        LabDepartment.Size = New Size(147, 32)
+        LabDepartment.TabIndex = 5
+        LabDepartment.Text = "Department:"
+        ' 
+        ' LabHours
+        ' 
+        LabHours.AutoSize = True
+        LabHours.Location = New Point(364, 379)
+        LabHours.Name = "LabHours"
+        LabHours.Size = New Size(243, 32)
+        LabHours.TabIndex = 6
+        LabHours.Text = "Hours worked (week):"
+        ' 
+        ' LabHourlyRate
+        ' 
+        LabHourlyRate.AutoSize = True
+        LabHourlyRate.Location = New Point(364, 433)
+        LabHourlyRate.Name = "LabHourlyRate"
+        LabHourlyRate.Size = New Size(144, 32)
+        LabHourlyRate.TabIndex = 7
+        LabHourlyRate.Text = "Hourly Rate:"
+        ' 
+        ' BtnAdd
+        ' 
+        BtnAdd.Location = New Point(919, 221)
+        BtnAdd.Name = "BtnAdd"
+        BtnAdd.Size = New Size(219, 46)
+        BtnAdd.TabIndex = 8
+        BtnAdd.Text = "Add"
+        BtnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' BtnDisplay
+        ' 
+        BtnDisplay.Location = New Point(919, 293)
+        BtnDisplay.Name = "BtnDisplay"
+        BtnDisplay.Size = New Size(219, 46)
+        BtnDisplay.TabIndex = 8
+        BtnDisplay.Text = "Display All"
+        BtnDisplay.UseVisualStyleBackColor = True
+        ' 
+        ' BtnPayReport
+        ' 
+        BtnPayReport.Location = New Point(919, 368)
+        BtnPayReport.Name = "BtnPayReport"
+        BtnPayReport.Size = New Size(219, 46)
+        BtnPayReport.TabIndex = 9
+        BtnPayReport.Text = "Pay Report"
+        BtnPayReport.UseVisualStyleBackColor = True
+        ' 
+        ' BtnSearch
+        ' 
+        BtnSearch.Location = New Point(919, 433)
+        BtnSearch.Name = "BtnSearch"
+        BtnSearch.Size = New Size(219, 46)
+        BtnSearch.TabIndex = 10
+        BtnSearch.Text = "Search"
+        BtnSearch.UseVisualStyleBackColor = True
+        ' 
+        ' BtnClear
+        ' 
+        BtnClear.Location = New Point(364, 1044)
+        BtnClear.Name = "BtnClear"
+        BtnClear.Size = New Size(774, 46)
+        BtnClear.TabIndex = 11
+        BtnClear.Text = "Clear"
+        BtnClear.UseVisualStyleBackColor = True
+        ' 
+        ' LstDisplay
+        ' 
+        LstDisplay.FormattingEnabled = True
+        LstDisplay.Location = New Point(364, 562)
+        LstDisplay.Name = "LstDisplay"
+        LstDisplay.Size = New Size(774, 452)
+        LstDisplay.TabIndex = 12
+        ' 
+        ' LabTitle
+        ' 
+        LabTitle.AutoSize = True
+        LabTitle.Location = New Point(630, 156)
+        LabTitle.Name = "LabTitle"
+        LabTitle.Size = New Size(231, 32)
+        LabTitle.TabIndex = 13
+        LabTitle.Text = "PlantRus - Time2Pay"
+        ' 
+        ' Time2Pay
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1544, 1134)
+        Controls.Add(LabTitle)
+        Controls.Add(LstDisplay)
+        Controls.Add(BtnClear)
+        Controls.Add(BtnSearch)
+        Controls.Add(BtnPayReport)
+        Controls.Add(BtnDisplay)
+        Controls.Add(BtnAdd)
+        Controls.Add(LabHourlyRate)
+        Controls.Add(LabHours)
+        Controls.Add(LabDepartment)
+        Controls.Add(LabEmployeeName)
+        Controls.Add(TxtHourlyRate)
+        Controls.Add(TxtHours)
+        Controls.Add(TxtDepartment)
+        Controls.Add(TxtEmployeeName)
+        Name = "Time2Pay"
+        Text = "Time2Pay"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TxtEmployeeName As TextBox
+    Friend WithEvents TxtDepartment As TextBox
+    Friend WithEvents TxtHours As TextBox
+    Friend WithEvents TxtHourlyRate As TextBox
+    Friend WithEvents LabEmployeeName As Label
+    Friend WithEvents LabDepartment As Label
+    Friend WithEvents LabHours As Label
+    Friend WithEvents LabHourlyRate As Label
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnDisplay As Button
+    Friend WithEvents BtnPayReport As Button
+    Friend WithEvents BtnSearch As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents LstDisplay As ListBox
+    Friend WithEvents LabTitle As Label
+
+End Class
---
+++ Folio Task 2/William_Bowley_U3O1-Folio-2-2024/William Bowley Sac 1 Filo 2 - Time to Pay/Form1.vb
@@ -0,0 +1,190 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+' Developer: William Bowley
+' Application: Time 2 Play Employee Manager
+' Version: 1
+' Date: 25/03/2024
+
+Public Class Time2Pay
+    ' DataBase file location
+    Const Time2PayDataBase As String = "DataBases/Time2PayData.txt"
+
+    ' This subroute displays all employee data to the LstDisplay when Display all button is pressed.
+    Private Sub BtnDisplay_Click(sender As Object, e As EventArgs) Handles BtnDisplay.Click
+        ' Validate that the database exists 
+        If Not My.Computer.FileSystem.FileExists(Time2PayDataBase) Then
+            MsgBox("Time2Pay: Database Not Found.")
+            Exit Sub
+        End If
+
+        ' Reads data from plain text to an array lines()
+        Dim StrLines() As String = File.ReadAllLines(Time2PayDataBase)
+
+        ' Clears List Box
+        LstDisplay.Items.Clear()
+
+        ' Reads 4 lines at a time to get employee data & Displays them.
+        For i As Integer = 0 To StrLines.Length - 4 Step 4
+            Dim StrName As String = StrLines(i)
+            Dim StrDepartment As String = StrLines(i + 1)
+            Dim IntHours As Integer = Integer.Parse(StrLines(i + 2))
+            Dim DblPayRate As Double = Double.Parse(StrLines(i + 3))
+
+            ' Displays employee data.
+            LstDisplay.Items.Add($"{StrName}: || Department: {StrDepartment} - Hours (week): {IntHours} - Rate: ${DblPayRate}/h")
+        Next
+    End Sub
+
+    ' This subroute displays the total employee amount, department and sorts them (highest -> lowest)
+    Private Sub BtnPayReport_Click(sender As Object, e As EventArgs) Handles BtnPayReport.Click
+        ' Clears List Box
+        LstDisplay.Items.Clear()
+
+        ' Validate that the database exists
+        If Not My.Computer.FileSystem.FileExists(Time2PayDataBase) Then
+            MsgBox("Time2Pay: Database Not Found.")
+            Exit Sub
+        End If
+
+        ' Reads data from plain text to an array lines()
+        Dim StrLines() As String = File.ReadAllLines(Time2PayDataBase)
+
+        ' Creates the Employee Data Dic & Department Data Dic
+        Dim DicEmployeesData As New List(Of Dictionary(Of String, Object))()
+        Dim DicDepartmentTotals As New Dictionary(Of String, Double)()
+
+        ' Initializes the department totals
+        DicDepartmentTotals.Add("Finance", 0)
+        DicDepartmentTotals.Add("Legal", 0)
+        DicDepartmentTotals.Add("Technology", 0)
+        DicDepartmentTotals.Add("Sales", 0)
+        DicDepartmentTotals.Add("Marketing", 0)
+
+        ' Reads 4 lines at a time to get employee data & Stores them in a list of dictionaries.
+        For i As Integer = 0 To StrLines.Length - 4 Step 4
+            ' Employee Values (Name, Department, Hours, PayRate, TotalPay)
+            Dim StrName As String = StrLines(i).Trim()
+            Dim StrDepartment As String = StrLines(i + 1).Trim()
+            Dim IntHours As Integer = Integer.Parse(StrLines(i + 2))
+            Dim DblPayRate As Double = Double.Parse(StrLines(i + 3))
+            Dim TotalPay As Double = IntHours * DblPayRate
+
+            ' Creates a New Dictionary for current employee.
+            Dim DicCurrentEmployee As New Dictionary(Of String, Object)()
+            DicCurrentEmployee.Add("name", StrName)
+            DicCurrentEmployee.Add("department", StrDepartment)
+            DicCurrentEmployee.Add("total_pay", TotalPay)
+            DicEmployeesData.Add(DicCurrentEmployee)
+
+            ' Add total pay to employee Department Pay
+            If DicDepartmentTotals.ContainsKey(StrDepartment) Then
+                DicDepartmentTotals(StrDepartment) += TotalPay
+            Else
+                MsgBox($"Department '{StrDepartment}' not found in departmentTotals.")
+            End If
+
+        Next
+
+        ' Sorts employee data by total pay (highest to lowest)
+        DicEmployeesData.Sort(Function(emp1, emp2) emp2("total_pay").CompareTo(emp1("total_pay")))
+
+        ' Display sorted employee data
+        LstDisplay.Items.Add("Sorted Employee Total Pay (Highest -> Lowest)")
+        For Each emp In DicEmployeesData
+            LstDisplay.Items.Add($"{emp("name")} ({emp("department")}): ${emp("total_pay"):F2}")
+        Next
+
+        ' Display department totals
+        LstDisplay.Items.Add("Department Totals:")
+        For Each dept In DicDepartmentTotals
+            LstDisplay.Items.Add($"Total for {dept.Key}: ${dept.Value:F2}")
+        Next
+    End Sub
+
+    ' This subroute does a linear search through the database for the employee name.
+    Private Sub BtnSearch_Click(sender As Object, e As EventArgs) Handles BtnSearch.Click
+        ' Gets Entered Employee Name
+        Dim StrLinearSearch As String = TxtEmployeeName.Text
+
+        ' Checks for a vaild Employee Name
+        If Not Regex.IsMatch(StrLinearSearch, "^[a-zA-Z\s]+$") OrElse String.IsNullOrEmpty(StrLinearSearch) Then
+            MsgBox("Time2Pay: Please enter a valid Employee Name.")
+            Exit Sub
+        End If
+
+        ' Validate that the database exists
+        If Not My.Computer.FileSystem.FileExists(Time2PayDataBase) Then
+            MsgBox("Time2Pay: Database Not Found.")
+            Exit Sub
+        End If
+
+        ' Clears List Box
+        LstDisplay.Items.Clear()
+
+        ' Reads data from plain text to an array lines()
+        Dim StrLines() As String = File.ReadAllLines(Time2PayDataBase)
+        Dim BolFound As Boolean = False
+
+        ' Reads 4 lines at a time to get employee data & Stores them in a list of dictionaries.
+        For i As Integer = 0 To StrLines.Length - 4 Step 4
+            Dim StrName As String = StrLines(i)
+            Dim BolIsFound As Boolean = StrName.Contains(StrLinearSearch)
+
+            ' If employee is Found than displays data.
+            If BolIsFound Then
+                Dim StrDepartment As String = StrLines(i + 1)
+                Dim IntHours As Integer = Integer.Parse(StrLines(i + 2))
+                Dim DblPayRate As Double = Double.Parse(StrLines(i + 3))
+                LstDisplay.Items.Add($"{StrName}: || Department: {StrDepartment} - Hours (week): {IntHours} - Rate: ${DblPayRate}/h")
+                BolFound = True
+            End If
+        Next
+
+        ' If no employees were found.
+        If Not BolFound Then
+            MsgBox("Time2Pay: We do employ this person.")
+        End If
+
+    End Sub
+
+    ' This subroute adds new employees to the database
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        ' Gets NewEmployee Values:
+        Dim StrName As String = TxtEmployeeName.Text
+        Dim StrDepartment As String = TxtDepartment.Text
+        Dim IntHours As Integer = Integer.Parse(TxtHours.Text)
+        Dim DblPayRate As Double = Double.Parse(TxtHourlyRate.Text)
+
+        ' Checks Week Hours between (1-60)
+        If IntHours < 1 OrElse IntHours > 60 Then
+            MsgBox("Time2Pay: Please enter a week work hours between (1-60 hours).")
+            Exit Sub
+        End If
+
+        ' Checks hourly rate between $20-$100
+        If DblPayRate < 20 OrElse DblPayRate > 100 Then
+            MsgBox("Time2Pay: Please enter a PayRate between ($20-100).")
+            Exit Sub
+        End If
+
+        ' Writes Data to file through Streamwriter.
+        Using writer As StreamWriter = New StreamWriter(Time2PayDataBase, True)
+            writer.WriteLine(StrName)
+            writer.WriteLine(StrDepartment)
+            writer.WriteLine(IntHours)
+            writer.WriteLine(DblPayRate)
+        End Using
+    End Sub
+
+    ' This subroute clears all User inputs
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        TxtEmployeeName.Clear()
+        TxtDepartment.Clear()
+        TxtHourlyRate.Clear()
+        TxtHours.Clear()
+        LstDisplay.Items.Clear()
+    End Sub
+End Class
